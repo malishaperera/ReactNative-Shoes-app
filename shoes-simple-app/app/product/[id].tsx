f5aee2c170a1e5e5aeebd8f2b7716e9ee5221084
Malisha Perera <malishashavinda577@gmail.com>
2025-03-01 03:03:02 +0530

    get product details screen call api

---
+++ shoes-simple-app/app/product/[id].tsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from 'react';
+import { View, ActivityIndicator } from 'react-native';
+import { useLocalSearchParams } from 'expo-router';
+import { Card } from "@/components/ui/card";
+import { Image } from "@/components//ui/image";
+import { VStack } from "@/components//ui/vstack";
+import { Heading } from "@/components//ui/heading";
+import { Box } from "@/components//ui/box";
+import { Button, ButtonText } from "@/components//ui/button";
+import { Text } from "@/components//ui/text";
+
+export default function ProductDetailsScreen() {
+  const { id } = useLocalSearchParams();
+  const [product, setProduct] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(`https://dummyjson.com/products/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setProduct(data);
+      })
+      .catch((error) => console.error('Error fetching product details:', error))
+      .finally(() => setLoading(false));
+  }, [id]);
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" color="#000" />
+      </View>
+    );
+  }
+
+  if (!product) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Product not found</Text>
+      </View>
+    );
+  }
+
+  return (
+    <Card  className="p-5 rounded-lg max-w-[560px] m-3 flex-1">
+
+    <Image
+      source={{
+        uri: product.thumbnail, 
+      }}
+      className="mb-6 h-[240px] w-full rounded-md aspect-[4/3]"
+      alt={`${product.title} image`}
+      style={{ width: '100%', height: 240 }}
+    />
+    <Text className="text-sm font-normal mb-2 text-typography-700">
+      {product.title}
+    </Text>
+    <VStack className="mb-6">
+      <Heading size="md" className="mb-4">
+        ${product.price}
+      </Heading>
+      <Text size="sm">
+        {product.description}
+      </Text>
+    </VStack>
+    <Box className="flex-col sm:flex-row">
+      <Button className="px-4 py-2 mr-0 mb-3 sm:mr-3 sm:mb-0 sm:flex-1">
+        <ButtonText size="sm">Add to cart</ButtonText>
+      </Button>
+      <Button variant="outline" className="px-4 py-2 border-outline-300 sm:flex-1">
+        <ButtonText size="sm" className="text-typography-600">
+          Wishlist
+        </ButtonText>
+      </Button>
+    </Box>
+  </Card>
+  );
+}
--- shoes-simple-app/components/ProductListItem.tsx
+++ shoes-simple-app/components/ProductListItem.tsx
@@ -5,39 +5,45 @@ import { Heading } from "./ui/heading";
 import { Box } from "./ui/box";
 import { Button, ButtonText } from "./ui/button";
 import { Text } from "./ui/text";
+import { Link } from "expo-router";
+import { Pressable } from "react-native";
 
 export default function ProductListItem({ product }) {
   return (
-    <Card className="p-5 rounded-lg max-w-[360px] m-3 flex-1">
-      <Image
-        source={{
-          uri: product.thumbnail, 
-        }}
-        className="mb-6 h-[240px] w-full rounded-md aspect-[4/3]"
-        alt={`${product.title} image`}
-        style={{ width: '100%', height: 240 }}
-      />
-      <Text className="text-sm font-normal mb-2 text-typography-700">
-        {product.title}
-      </Text>
-      <VStack className="mb-6">
-        <Heading size="md" className="mb-4">
-          ${product.price}
-        </Heading>
-        <Text size="sm">
-          {product.description}
+    <Link href={`product/${product.id}`} asChild>
+      <Pressable className="flex-1">
+      <Card  className="p-5 rounded-lg max-w-[360px] m-3 flex-1">
+        <Image
+          source={{
+            uri: product.thumbnail, 
+          }}
+          className="mb-6 h-[240px] w-full rounded-md aspect-[4/3]"
+          alt={`${product.title} image`}
+          style={{ width: '100%', height: 240 }}
+        />
+        <Text className="text-sm font-normal mb-2 text-typography-700">
+          {product.title}
         </Text>
-      </VStack>
-      <Box className="flex-col sm:flex-row">
-        <Button className="px-4 py-2 mr-0 mb-3 sm:mr-3 sm:mb-0 sm:flex-1">
-          <ButtonText size="sm">Add to cart</ButtonText>
-        </Button>
-        <Button variant="outline" className="px-4 py-2 border-outline-300 sm:flex-1">
-          <ButtonText size="sm" className="text-typography-600">
-            Wishlist
-          </ButtonText>
-        </Button>
-      </Box>
-    </Card>
+        
+          <Heading size="md" className="mb-4">
+            ${product.price}
+          </Heading>
+          <Text size="sm">
+            {product.description}
+          </Text>
+        
+        {/* <Box className="flex-col sm:flex-row">
+          <Button className="px-4 py-2 mr-0 mb-3 sm:mr-3 sm:mb-0 sm:flex-1">
+            <ButtonText size="sm">Add to cart</ButtonText>
+          </Button>
+          <Button variant="outline" className="px-4 py-2 border-outline-300 sm:flex-1">
+            <ButtonText size="sm" className="text-typography-600">
+              Wishlist
+            </ButtonText>
+          </Button>
+        </Box> */}
+      </Card>
+      </Pressable>
+    </Link>
   );
 }
