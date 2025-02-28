20960bee1b7022d1bbd7f88be1216c564c3988e0
Malisha Perera <malishashavinda577@gmail.com>
2025-03-01 00:28:22 +0530

    install gluestack

--- shoes-simple-app/app/_layout.tsx
+++ shoes-simple-app/app/_layout.tsx
@@ -1,9 +1,10 @@
-import { Stack ,Slot,Tabs} from "expo-router";
-
-
+import "@/global.css";
+import { Stack } from "expo-router";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 export default function RootLayout() {
-    return(
-        <Stack />
-    )
-    
+  return (
+    <GluestackUIProvider>
+      <Stack />
+    </GluestackUIProvider>
+  );
 }
--- shoes-simple-app/app/index.tsx
+++ shoes-simple-app/app/index.tsx
@@ -1,15 +1,23 @@
 import { View, Text, FlatList } from "react-native";
 import products from "../assets/products.json";
 import ProductListItem from "../components/ProductListItem";
+import { Button, ButtonText } from "@/components/ui/button";
 
 
 
 export default function HomeScreen() {
   return (
-    <FlatList
-      data={products}
-      renderItem={({ item }) => <ProductListItem product={item} />}
-      keyExtractor={(item) => item.id.toString()} // Ensure each item has a unique key
-    />
+    <>
+      <Button variant="outline">
+        <ButtonText>Press Me</ButtonText>
+      </Button>
+
+      {/* <FlatList
+        data={products}
+        renderItem={({ item }) => <ProductListItem product={item} />}
+        keyExtractor={(item) => item.id.toString()} // Ensure each item has a unique key
+      /> */}
+    </>
+
   );
 }
